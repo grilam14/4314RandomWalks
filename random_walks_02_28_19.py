8b0980f43032846d8fc113d6abe39cfda85f46e0
Julio <jlopeziv45@gmail.com>
2019-04-03 13:20:39 -0600

    Multiple agents can run

--- random_walks_02_28_19.py
+++ random_walks_02_28_19.py
@@ -12,7 +12,70 @@ Generate set of points, go through list of points to see if our location is with
 '''
 
 class Random_Walks_Python():
-    def random_walks(self):
+    def __init__(self):
+                # Our code start
+        min = -100
+        max = 100
+        # better if food has an integer sqrt
+        food = 49
+        # better if clusterCount is divisible by food
+        clusterCount = 7
+        clusterPoints = round(food/clusterCount)
+        clusterRange = 15
+        self.eatRange = 10
+
+        self.xRandom = collections.deque()
+        self.yRandom = collections.deque()
+        self.xUniform = collections.deque()
+        self.yUniform = collections.deque()
+        self.xCluster = collections.deque()
+        self.yCluster = collections.deque()
+
+        # generate random points
+        for _ in range(food):
+            self.xRandom.append(np.random.uniform(min, max, 1))
+            self.yRandom.append(np.random.uniform(min, max, 1))
+            
+
+        # generate evenly spaced points
+        xlen = np.linspace(min, max, round(np.sqrt(food)))
+        ylen = np.linspace(min, max, round(np.sqrt(food)))
+        for i in range(len(xlen)):
+            for j in range(len(ylen)):
+                self.xUniform.append(xlen[i])
+                self.yUniform.append(ylen[j])
+
+        # generate random w/ cluster points
+        for _ in range(clusterCount): 
+            xrand = np.random.uniform(min, max, 1)
+            yrand = np.random.uniform(min, max, 1)
+            for _ in range(clusterPoints):
+                self.xCluster.append(np.random.uniform(xrand - clusterRange, xrand + clusterRange, 1))
+                self.yCluster.append(np.random.uniform(yrand - clusterRange, yrand + clusterRange, 1))
+
+
+        ##USE THIS TO CHANGE DATA STRUCTURE
+
+        self.xFoodDistribution = self.xCluster
+        self.yFoodDistribution = self.yCluster
+        # Our code end
+        # Hello world
+
+    def change_distribution(self, dist_name):
+        if dist_name == "random":
+            self.xFoodDistribution = self.xRandom
+            self.yFoodDistribution = self.yRandom
+
+        elif dist_name == "uniform":
+            self.xFoodDistribution = self.xUniform
+            self.yFoodDistribution = self.yUniform
+
+        elif dist_name == "cluster":
+            self.xFoodDistribution = self.xCluster
+            self.yFoodDistribution = self.yCluster
+        
+
+    def random_walks(self, fig_cnt):
         N = 500 #no of steps per trajectory
         realizations = 1 #number of trajectories
         v = 1.0 #velocity (step size)
@@ -22,55 +85,6 @@ class Random_Walks_Python():
         w_array = [0.0]
         ratio_theta_s_brw_crw = 1
         plot_walks = 1
-        count = 0
-
-        # Our code start
-        min = -100
-        max = 100
-        # better if food has an integer sqrt
-        food = 49
-        # better if clusterCount is divisible by food
-        clusterCount = 7
-        clusterPoints = round(food/clusterCount)
-        clusterRange = 15
-        eatRange = 10
-
-        xRandom = collections.deque()
-        yRandom = collections.deque()
-        xUniform = collections.deque()
-        yUniform = collections.deque()
-        xRandomCluster = collections.deque()
-        yRandomCluster = collections.deque()
-
-        # generate random points
-        for _ in range(food):
-            xRandom.append(np.random.uniform(min, max, 1))
-            yRandom.append(np.random.uniform(min, max, 1))
-            
-
-        # generate evenly spaced points
-        xlen = np.linspace(min, max, round(np.sqrt(food)))
-        ylen = np.linspace(min, max, round(np.sqrt(food)))
-        for i in range(len(xlen)):
-            for j in range(len(ylen)):
-                xUniform.append(xlen[i])
-                yUniform.append(ylen[j])
-
-        # generate random w/ cluster points
-        for _ in range(clusterCount): 
-            xrand = np.random.uniform(min, max, 1)
-            yrand = np.random.uniform(min, max, 1)
-            for _ in range(clusterPoints):
-                xRandomCluster.append(np.random.uniform(xrand - clusterRange, xrand + clusterRange, 1))
-                yRandomCluster.append(np.random.uniform(yrand - clusterRange, yrand + clusterRange, 1))
-
-
-        ##USE THIS TO CHANGE DATA STRUCTURE
-
-        xFoodDistribution = xRandomCluster
-        yFoodDistribution = yRandomCluster
-        # Our code end
-        # Hello world
     
 
         
@@ -80,19 +94,20 @@ class Random_Walks_Python():
             for theta_s_i in range(len(theta_s_array)):
                 theta_s_crw = np.multiply(ratio_theta_s_brw_crw,theta_s_array[theta_s_i])
                 theta_s_brw = theta_s_array[theta_s_i]
-                x,y, xFoodsEaten, yFoodsEaten = self.BRCW(N, realizations, v, theta_s_crw, theta_s_brw, w, xFoodDistribution, yFoodDistribution, eatRange)
+                x,y, xFoodsEaten, yFoodsEaten = self.BRCW(N, realizations, v, theta_s_crw, theta_s_brw, w, self.xFoodDistribution, self.yFoodDistribution, self.eatRange)
                 if plot_walks == 1:
-                    count += 1
-                    plt.figure(count)
+                    fig = plt.figure(fig_cnt)
                     plt.title("w: " + str(w) + " theta: " + str(theta_s_array[theta_s_i]))
                     plt.plot(x.T, y.T)
-                    plt.scatter(xRandomCluster, yRandomCluster)
+                    plt.scatter(self.xFoodDistribution, self.yFoodDistribution)
                     plt.scatter(xFoodsEaten, yFoodsEaten, color="red")
                     plt.axis('equal')
+
+                    fig.savefig("figures/animal_"+str(fig_cnt)+".png")
                 efficiency_array[theta_s_i, w_i] = np.divide(np.mean(x[:,-1]-x[:,0]),(v*N))
                 print(efficiency_array[theta_s_i, w_i])
-            plt.show()
-        plt.figure()
+            #plt.show()
+        #plt.figure()
         legend_array = []
         w_array_i = np.repeat(w_array,len(efficiency_array))
         for theta_s_i in range(0, len(theta_s_array)):
@@ -130,10 +145,10 @@ class Random_Walks_Python():
                 X[realization_i, step_i] = X[realization_i][step_i-1] + (v * (w*math.cos(theta_brw))) + ((1-w) * math.cos(theta_crw))
                 Y[realization_i, step_i] = Y[realization_i][step_i-1] + (v* (w*math.sin(theta_brw))) +((1-w)* math.sin(theta_crw))
 
-                index = 0;
+                index = 0
                 eaten = False
                 for food_x, food_y in zip(xFoods, yFoods):
-                    print(food_x, food_y)
+                    # print(food_x, food_y)
                     # if the animal is within the range of a piece of food
                     if(food_x - eatRange <= X[realization_i, step_i] <= food_x + eatRange and food_y - eatRange <= Y[realization_i, step_i] <= food_y + eatRange):
                         xFoodsEaten.append(food_x)
@@ -141,10 +156,6 @@ class Random_Walks_Python():
                         eaten = True
                         break
                     index+=1
-                # remove food
-                if(eaten):
-                    del xFoods [index]
-                    del yFoods [index]
 
                 current_x_disp = X[realization_i][step_i] - X[realization_i][step_i-1]
                 current_y_disp = Y[realization_i][step_i] - Y[realization_i][step_i-1]
@@ -155,4 +166,12 @@ class Random_Walks_Python():
         return X, Y, xFoodsEaten, yFoodsEaten
 
 rdm_plt = Random_Walks_Python()
-rdm_plt.random_walks()
+
+dists = ['random', 'uniform', 'cluser']
+j = 1
+for dist in dists:
+    rdm_plt.change_distribution(dist)
+    for i in range(1,3):
+        rdm_plt.random_walks(j)
+        j += 1
+
