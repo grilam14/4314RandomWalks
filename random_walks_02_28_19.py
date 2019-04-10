b370b505e746f8b18adf8315689b01e1e1839c30
Julio Lopez <julo4925@colorado.edu>
2019-04-10 12:56:20 -0600

    Added smelly smell

--- random_walks_02_28_19.py
+++ random_walks_02_28_19.py
@@ -17,12 +17,13 @@ class Random_Walks_Python():
         min = -100
         max = 100
         # better if food has an integer sqrt
-        food = 49
+        food = 36
         # better if clusterCount is divisible by food
-        clusterCount = 7
+        clusterCount = 3
         clusterPoints = round(food/clusterCount)
         clusterRange = 15
-        self.eatRange = 10
+        self.eatRange = 1
+        self.scentRange = 10
 
         self.xRandom = collections.deque()
         self.yRandom = collections.deque()
@@ -75,7 +76,7 @@ class Random_Walks_Python():
             self.yFoodDistribution = self.yCluster
         
 
-    def random_walks(self, fig_cnt):
+    def random_walks(self, fig_cnt, plot_walks=1):
         N = 500 #no of steps per trajectory
         realizations = 1 #number of trajectories
         v = 1.0 #velocity (step size)
@@ -84,7 +85,6 @@ class Random_Walks_Python():
         #w_array = [0.0, 0.5, 1.0] #w is the weighting given to the directional bias (and hence (1-w) is the weighting given to correlated motion)
         w_array = [0.0]
         ratio_theta_s_brw_crw = 1
-        plot_walks = 1
     
 
         
@@ -94,11 +94,15 @@ class Random_Walks_Python():
             for theta_s_i in range(len(theta_s_array)):
                 theta_s_crw = np.multiply(ratio_theta_s_brw_crw,theta_s_array[theta_s_i])
                 theta_s_brw = theta_s_array[theta_s_i]
-                x,y, xFoodsEaten, yFoodsEaten = self.BRCW(N, realizations, v, theta_s_crw, theta_s_brw, w, self.xFoodDistribution, self.yFoodDistribution, self.eatRange)
-                if plot_walks == 1:
-                    fig = plt.figure(fig_cnt)
+                x,y, xFoodsEaten, yFoodsEaten = self.BRCW(  N, realizations, v, theta_s_crw, theta_s_brw, 
+                                                            w, self.xFoodDistribution, self.yFoodDistribution, 
+                                                            self.eatRange, self.scentRange)
+                if plot_walks:
+                    plt.figure(fig_cnt)
                     plt.title("w: " + str(w) + " theta: " + str(theta_s_array[theta_s_i]))
                     plt.plot(x.T, y.T)
+                    plt.plot([-100, 100, 100,-100,-100], [100,100,-100,-100,100], c="k",
+                             linestyle='--', linewidth=0.5)
                     plt.scatter(self.xFoodDistribution, self.yFoodDistribution)
                     plt.scatter(xFoodsEaten, yFoodsEaten, color="red")
                     plt.axis('equal')
@@ -106,7 +110,7 @@ class Random_Walks_Python():
                     #fig.savefig("figures/animal_"+str(fig_cnt)+".png")
                 efficiency_array[theta_s_i, w_i] = np.divide(np.mean(x[:,-1]-x[:,0]),(v*N))
                 #print(efficiency_array[theta_s_i, w_i])
-            plt.show()
+
         #plt.figure()
         legend_array = []
         w_array_i = np.repeat(w_array,len(efficiency_array))
@@ -125,9 +129,27 @@ class Random_Walks_Python():
         '''
 
 
+    def scent(self, curr_x, curr_y, x_food, y_food, x_eaten, y_eaten, scentRange):
+        closest_food_dist = 1000
+        closest_food_coor = [-200, -200]
+        for x, y in zip(x_food, y_food):
+            dist_to_food = np.sqrt((curr_x-x)**2 + (curr_y-y)**2)
+            if dist_to_food < closest_food_dist and (x[0] not in x_eaten) and (y[0] not in y_eaten):
+                closest_food_dist = dist_to_food
+                closest_food_coor = [x[0],y[0]]
+        
+        if closest_food_dist < scentRange:
+            x_disp = closest_food_coor[0] - curr_x
+            y_disp = closest_food_coor[1] - curr_y
+            direction = math.atan2(y_disp,x_disp)
+            return 1, direction, direction
+
+        else:
+            return 0,1,1
+
 
 #The funciton generate 2D Biased Corrolated Random Walks
-    def BRCW(self,N, realizations, v, theta_s_crw, theta_s_brw,w, xFoods, yFoods, eatRange):
+    def BRCW(self,N, realizations, v, theta_s_crw, theta_s_brw,w, xFoods, yFoods, eatRange, scentRange):
         X = np.zeros([realizations, N])
         Y = np.zeros([realizations, N])
         theta = np.zeros([realizations, N])
@@ -139,7 +161,12 @@ class Random_Walks_Python():
 
         for realization_i in range(realizations):
             for step_i in range(1,N):
-
+                
+                foodNear, theta_c, theta_b = self.scent(X[realization_i, step_i-1],
+                                                        Y[realization_i, step_i-1],
+                                                        xFoods, yFoods, 
+                                                        xFoodsEaten, yFoodsEaten, scentRange)
+                foodNear = 0
                 if( X[realization_i, step_i-1] >= 100):
                     theta_crw = -np.pi
                     theta_brw = -np.pi
@@ -149,12 +176,16 @@ class Random_Walks_Python():
                     theta_brw = 0
 
                 elif( Y[realization_i, step_i-1] >= 100):
-                    theta_crw = -1
-                    theta_brw = -1
+                    theta_crw = -np.pi/2
+                    theta_brw = -np.pi/2
 
                 elif( Y[realization_i, step_i-1] <= -100):
-                    theta_crw = 1
-                    theta_brw = 1
+                    theta_crw = np.pi/2
+                    theta_brw = np.pi/2
+
+                elif (foodNear):
+                    theta_crw = theta_c
+                    theta_brw = theta_b
 
                 else:
                     theta_crw = theta[realization_i][step_i-1]+(theta_s_crw* 2.0 * (np.random.rand(1,1)-0.5))
@@ -168,7 +199,7 @@ class Random_Walks_Python():
                 for food_x, food_y in zip(xFoods, yFoods):
                     # print(food_x, food_y)
                     # if the animal is within the range of a piece of food
-                    if(food_x - eatRange <= X[realization_i, step_i] <= food_x + eatRange and food_y - eatRange <= Y[realization_i, step_i] <= food_y + eatRange):
+                    if(food_x - eatRange <= X[realization_i, step_i] <= food_x + eatRange and food_y - eatRange <= Y[realization_i, step_i] <= food_y + eatRange) and (food_x not in xFoodsEaten) and (food_y not in yFoodsEaten):
                         xFoodsEaten.append(food_x)
                         yFoodsEaten.append(food_y)
                         eaten = True
@@ -180,16 +211,16 @@ class Random_Walks_Python():
                 current_direction = math.atan2(current_y_disp,current_x_disp)
 
                 theta[realization_i, step_i] = current_direction
-
+        print(len(xFoodsEaten))
         return X, Y, xFoodsEaten, yFoodsEaten
 
 rdm_plt = Random_Walks_Python()
 
-dists = ['random', 'uniform', 'cluster']
 
-for dist in dists:
-    rdm_plt.change_distribution(dist)
-    for i in range(1,3):
-        rdm_plt.random_walks(i)
+rdm_plt.change_distribution('random')
+for i in range(1,20):
+    rdm_plt.random_walks(i, plot_walks=0)
+
+plt.show()
 
 
